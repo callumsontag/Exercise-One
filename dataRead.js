679e0e9619964e5f18cf6fc0b79ca558d40e3a03
Callum Sontag <callum.sontag@mettle.co.uk>
2023-04-18 11:06:17 +0100

    added functions to find oldest single male and female

--- dataRead.js
+++ dataRead.js
@@ -21,9 +21,25 @@ const normaliseData = (csvData) => {
 
 const marriedAndTwenties = (people) => people.filter((person) =>  person.age > 19 && person.age < 30 && person.maritalStatus === 'Married').length
 
+const oldestSingleMale = (people) => {
+    const singleMale = people.filter((person) => person.maritalStatus === 'Single' && person.gender === 'Male')
+    const ageStringToNum = singleMale.map(person => ({ ...person, age: parseInt(person.age) }))
+    const oldestSingleMale = ageStringToNum.reduce((oldest, current) => current.age > oldest.age ? current : oldest, { age: -Infinity });
+    return oldestSingleMale
+}
+
+const oldestSingleFemale = (people) => {
+    const singleFemale = people.filter((person) => person.maritalStatus === 'Single' && person.gender === 'Female')
+    const ageStringToNum = singleFemale.map(person => ({ ...person, age: parseInt(person.age) }))
+    const oldestSingleFemale = ageStringToNum.reduce((oldest, current) => current.age > oldest.age ? current : oldest, { age: -Infinity });
+    return oldestSingleFemale
+}
+
 
 const data = readData('./data.csv')
 
 const people = normaliseData(data)
 
 console.log(marriedAndTwenties(people))
+console.log(oldestSingleMale(people))
+console.log(oldestSingleFemale(people))
